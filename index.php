2b60243fe19aea869bb31d52c31864566f449f20
Sarthak sadh <sarthaksadh01@gmail.com>
2019-06-21 16:04:18 +0530

    navbar and intro section completed

---
+++ index.php
@@ -0,0 +1,240 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>GGSIPU</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+
+  <!-- Google Fonts -->
+  <link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,500,600,700,700i|Montserrat:300,400,500,600,700"
+    rel="stylesheet">
+
+  <!-- Bootstrap CSS File -->
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
+  <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+  <!-- =======================================================
+   
+  
+     Nav Bar Section
+
+
+  ======================================================= -->
+
+  <header id="header">
+    <div class="container">
+
+      <div class="logo float-left">
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <h1 class="text-light"><a href="#intro" class="scrollto"><span>GGSIPU</span></a></h1>
+        <!-- <a href="#header" class="scrollto"><img src="img/logo.png" alt="" class="img-fluid"></a> -->
+      </div>
+
+      <nav class="main-nav float-right d-none d-lg-block">
+        <ul>
+          <li class="drop-down "><a href="#">Academics</a>
+
+            <ul>
+
+              <li class="drop-down"><a href="#">Schools & Programs</a>
+                <ul>
+                  <li><a href="#">List of Programs</a></li>
+                  <li><a href="#">University Schools</a></li>
+                  <li><a href="#">University Centres</a></li>
+                  <li><a href="#">Affiliated Institutes</a></li>
+                  <li><a href="#">Admission Branch</a></li>
+                </ul>
+              </li>
+              <li class="drop-down"><a href="#">Academic</a>
+                <ul>
+                  <li><a href="#">Academic Affairs</a></li>
+                  <li><a href="#">Scheme/Syllabus</a></li>
+                  <li><a href="#">International Affairs</a></li>
+                  <li><a href="#">Research & Consultancy</a></li>
+                  <li><a href="#">Admission 2019</a></li>
+                </ul>
+              </li>
+            </ul>
+
+
+          </li>
+          <li class="drop-down "><a href="#">Examination</a>
+
+            <ul>
+
+              <li><a href="#">Examination Notices</a></li>
+              <li><a href="#">Examination Results</a></li>
+              <li><a href="#">Regulation Forms</a></li>
+              <li><a href="#">Examination Datesheet</a></li>
+              <li><a href="#">NAD Information</a></li>
+              <li><a href="#">NAD registration on CVL</a></li>
+              <li><a href="#">NAD registration on NDML</a></li>
+
+              <!--  -->
+
+
+            </ul>
+
+
+          </li>
+          <li class="drop-down "><a href="#">Administration</a>
+
+            <ul>
+
+              <li><a href="#">Accounts</a></li>
+              <li><a href="#"> Academic Coordination</a></li>
+              <li><a href="#">Acts, Statutes, Ordinances</a></li>
+              <li><a href="#"> Statutory Bodies</a></li>
+              <li><a href="#"> Affiliation Branch</a></li>
+              <li><a href="#">Employee Resources</a></li>
+              <li><a href="#"> Office of the Proctor</a></li>
+              <li><a href="#">Estate & Security Branch</a></li>
+              <li><a href="#">Misc. Notices / Circulars</a></li>
+              <li><a href="#">Personnel Branch</a></li>
+              <li><a href="#">Planning and Policy</a></li>
+              <li><a href="#">Purchase Branch</a></li>
+              <li><a href="#">Staff Development Cell</a></li>
+              <li><a href="#">University Works Division</a></li>
+
+            </ul>
+
+
+
+          </li>
+          <li class="drop-down "><a href="#">Campus Life</a>
+
+            <ul>
+
+              <li class="drop-down"><a href="#">Facilities</a>
+                <ul>
+                  <li><a href="#">Students Zone</a></li>
+                  <li><a href="#">Hostels/Facilities</a></li>
+                  <li><a href="#">Students Council</a></li>
+
+
+                </ul>
+              </li>
+              <li class="drop-down"><a href="#">Clubs/Activities</a>
+
+                <ul>
+                  <li><a href="#"> Sports Activities</a></li>
+                  <li><a href="#"> Cultural Activities</a></li>
+                  <li><a href="#"> NSS Cell</a></li>
+
+                </ul>
+              </li>
+            </ul>
+
+
+          </li>
+          <li class="drop-down"><a href="#">Events & Notices</a>
+
+
+            <ul>
+
+              <li class="drop-down"><a href="#">News & Events</a>
+                <ul>
+                  <li><a href="#"> Jobs & Opportunites</a></li>
+                  <li><a href="#"> Newsletter</a></li>
+                  <li><a href="#"> Seminar/Conference</a></li>
+                  <li><a href="#">Placement Cell</a></li>
+                </ul>
+              </li>
+              <li class="drop-down"><a href="#">Information</a>
+
+
+                <ul>
+                  <li><a href="#"> Notices/Circulars</a></li>
+                  <li><a href="#"> RTI Act 2005</a></li>
+                  <li><a href="#"> Tenders</a></li>
+                  <li><a href="#"> Differently Abled Student</a></li>
+
+                </ul>
+              </li>
+            </ul>
+
+
+          </li>
+
+          <li> <input class="form-control mr-sm-2" type="search" placeholder="    Search links" aria-label="Search">
+          </li>
+
+
+
+
+          <!-- <li><a href="#footer">Contact Us</a></li> -->
+        </ul>
+      </nav><!-- .main-nav -->
+
+    </div>
+  </header><!-- #header -->
+
+  <section id="intro" class="clearfix">
+    <div class="container d-flex h-100">
+      <div style="text-align: center;" class="row justify-content-center align-self-center">
+
+        <div class="ggsipu-button intro-info">
+          <br><br>
+          <div class="text-center ">
+            <a href="#" class="ggsipu-btn btn-lg btn-get-started scrollto">Explore GGSIPU</a>
+          </div>
+
+        </div>
+
+
+      </div>
+
+    </div>
+  </section>
+
+
+
+  <!-- =======================================================
+   
+  
+    Add other Sections below
+
+
+  ======================================================= -->
+
+
+  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+  <!-- Uncomment below if you want to use a preloader -->
+  <!-- <div id="preloader"></div> -->
+
+  <!-- JavaScript Libraries -->
+  <script src="lib/jquery/jquery.min.js"></script>
+  <script src="lib/jquery/jquery-migrate.min.js"></script>
+  <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="lib/easing/easing.min.js"></script>
+  <script src="lib/mobile-nav/mobile-nav.js"></script>
+  <script src="lib/wow/wow.min.js"></script>
+  <script src="lib/waypoints/waypoints.min.js"></script>
+  <script src="lib/counterup/counterup.min.js"></script>
+  <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+  <script src="lib/isotope/isotope.pkgd.min.js"></script>
+  <script src="lib/lightbox/js/lightbox.min.js"></script>
+
+  <!-- All animation Javascript File -->
+  <script src="js/main.js"></script>
+
+</body>
+
+</html>
